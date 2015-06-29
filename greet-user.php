180104624517b0c4ede0931557850ec6546995e7
Yan Wu <ywu@ahbelo.com>
2015-06-29 10:31:28 -0500

    initial commit

---
+++ greet-user.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Greet User
+ * Author: Peter Suhm
+ * Version: 0.0.1
+ */
+$checkForGreetingFilter = function() {
+    if (isset($_GET['hello'])) {
+        wp_die("<h1>Hello {$_GET['hello']}!</h1>");
+    }
+};
+add_filter('wp_loaded', $checkForGreetingFilter);
